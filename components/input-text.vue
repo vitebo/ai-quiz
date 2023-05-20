5ec78d871a8ac69ae4693e9366f730f9c5cc0aa9
vitebo <vitebo@hotmail.com>
2023-05-20 14:53:41 -0300

    feat(components): add InputText component and use it in index page
    This commit adds a new InputText component that handles user input and emits an event to update the model value. The component is then used in the index page instead of a regular text input. Additionally, a button with custom styles has been added to the form.

---
+++ components/input-text.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const emit = defineEmits(["update:modelValue"]);
+
+defineProps({
+  placeholder: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
+  modelValue: {
+    type: String,
+    required: true,
+  },
+});
+
+function handleInput(event: Event) {
+  const target = event.target as HTMLInputElement;
+  emit("update:modelValue", target.value);
+}
+</script>
+
+<template>
+  <input
+    type="text"
+    class="input input-bordered w-full max-w-xs"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="handleInput"
+  />
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,14 @@ const quizTheme = ref<string>("");
 </script>
 
 <template>
-  <div>
+  <div class="min-h-screen">
     <h1>Ai Quiz</h1>
 
     <form>
       <label>Selecione o tema do quiz</label>
-      <input name="quiz-theme" type="text" v-model="quizTheme" />
+      <InputText name="quiz-theme" v-model="quizTheme" />
       <button type="submit">Iniciar</button>
+      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">Button</button>
     </form>
   </div>
 </template>
