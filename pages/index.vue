0c0a8382d6ecca98d408fb805e2e6de203a3a085
vitebo <vitebo@hotmail.com>
2023-05-20 13:17:57 -0300

    feat(quiz): add quiz theme selection and start button to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+const quizTheme = ref<string>("");
+</script>
+
 <template>
   <div>
-    Ai Quiz
+    <h1>Ai Quiz</h1>
+
+    <form>
+      <label>Selecione o tema do quiz</label>
+      <input name="quiz-theme" type="text" v-model="quizTheme" />
+      <button type="submit">Iniciar</button>
+    </form>
   </div>
 </template>
--- test/pages/index.test.ts
+++ test/pages/index.test.ts
@@ -4,7 +4,18 @@ import { $fetch, setup } from '@nuxt/test-utils'
 describe('pages/index', async () => {
   await setup();
   
-  it('Renders Hello Nuxt', async () => {
+  it('Renders application title', async () => {
     expect(await $fetch('/')).toMatch('Ai Quiz')
   })
+
+  it('Render an input to set the quiz theme', async () => {
+    const html = await $fetch('/');
+    expect(html).toMatch('Selecione o tema do quiz');
+    expect(html).toMatch('<input name="quiz-theme" type="text" value="">');
+  });
+
+  it('Render a button to start the quiz', async () => {
+    const html = await $fetch('/');
+    expect(html).toMatch('<button type="submit">Iniciar</button>');
+  });
 })
